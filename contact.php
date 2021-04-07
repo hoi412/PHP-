d0c5f7be55174c2dfe50708d400bf884791f7e49
hoi412 <58466125+hoi412@users.noreply.github.com>
2021-04-07 17:25:05 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,92 @@
+<html>
+<head>
+<title>Contact Kalsbeek College</title>
+</head>
+<body>
+<img src="hot.jpg" class="centerImage" alt="kalsbeek logo" width="300" height="120">
+
+<img src="Kalsbeek_maps.png" class="bottomImage" alt="kaart van locatie kalsbeek" width="450" height="280">
+</body>
+<style>
+body {
+  background-color: #23a73a;
+}
+
+.Enter
+{  background: #f5b1bc;
+   border: none;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  position:fixed;
+  bottom:28px;
+  left:42.75%;
+  cursor: pointer;
+  border: 1px solid white;}
+
+.centerImage {
+ padding-left: 630px;
+}
+
+.bottomImage{
+    position:fixed;
+    bottom:30px;
+    left:38%;
+}
+
+  .text{
+  color: black;
+  text-align: center;
+  font-size: 20px;
+  padding-left: 320px;
+  padding-right: 300px;
+  padding-top: 20px;
+  }
+
+.sidenav {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: green;
+  overflow-x: hidden;
+  padding-top: 20px;
+  border: 2px solid white;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: white;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: white;
+}
+</style>
+
+<?php
+session_start();
+?>
+<div class="sidenav">
+  <a href="https://chatroom--n333n444.repl.co/">Gastenboek</a>
+  <a href="https://chatroom--n333n444.repl.co/contact.php">Contact</a>
+ </div>
+
+<div class = "text">
+<p><h1> Contact </h1></p>
+
+<p>Het Kalsbeek College is een christelijke scholengemeenschap in het voortgezet onderwijs in de Nederlandse stad Woerden met meer dan 3.000 leerlingen. Wilt u contact met ons opnemen? De onderstaande contactgegevens helpen u verder.<p>
+<p><b>Telefoonnummer: 0348 417 694</p></b>
+<p><b>Adres: Jozef Israelslaan 56, 3443 CT Woerden</p></b>
+<p><b>Mail: contact_spk@kalsbeek.nl</p></b>
+</div>">Gastenboek</a>
+  <a href="#clients">Contact</a>
+ </div>
+
+
